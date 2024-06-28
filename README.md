48510ed8ee2fbf77d3b20f05086e2edf7ad944da
qboy <guow.qiu@gmail.com>
2024-06-28 22:21:28 +0800

    add readme.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # suricata-rules-awesome
+> 用于收集suricata 的github上的规则
+> 批量更新
+```
+git submodule foreach git pull
+```
---
+++ merge_rules.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# git submodule foreach git pull
+d=$(date +%Y-%m-%d)
+echo $d
+rm -f local.rules
+
+echo "# merge by $d" >> local.rules
+echo '
+' >> local.rules
+
+echo "#1. cobaltstrike-suricata-rules" >> local.rules
+cat ./cobaltstrike-suricata-rules/suricata/*.rules >> local.rules
+
+echo '
+' >> local.rules
+echo "#2. hunting-rules" >> local.rules
+cat ./hunting-rules/hunting.rules >> local.rules
+
+echo '
+' >> local.rules
+echo "#3. JJyyy/suricata-rules" >> local.rules
+cat ./JJyyy/surica-rules/*.rules >> local.rules
+
+echo '
+' >> local.rules
+echo "#4. suricata-rules" >> local.rules
+cat ./suricata-rules/suricata-ids.rules >> local.rules
+
+echo '
+' >> local.rules
+echo "#5. Suricata-Detect-DoS-Attack" >> local.rules
+cat ./Suricata-Detect-DoS-Attack/*.rules >> local.rules
+
+
+# echo '
+# ' >> local.rules
+# echo "#6. suricata-rules" >> local.rules
+# cat ./suricata-rules/suricata-rules.rules >> local.rules
+
+
+git add local.rules
+git commit -m "merge by $d"
+git push
